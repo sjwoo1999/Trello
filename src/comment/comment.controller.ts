12a4612a8e5f11bcf3bf2868dde6665d3f40a182
sinhong jeong <jsh719803@gmail.com>
2024-03-21 18:55:11 +0900

    🔨 CommentController에 CRUD 작업 및 라우팅 추가
    - CommentController에 댓글 생성, 조회, 수정, 삭제에 대한 API 엔드포인트 및 라우팅 추가함.

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,34 +1,72 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { BoardIdMemberDto } from '../member/dto/boardId-member.dto';
 
 @Controller('comment')
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
 
-  @Post()
-  create(@Body() createCommentDto: CreateCommentDto) {
-    return this.commentService.create(createCommentDto);
+  // 댓글 작성 API 경로
+  @Post('cards/:cardId/comment')
+  createComment(
+    @Param('cardId') cardId: string,
+    @Body() createCommentDto: CreateCommentDto,
+    @Body() member: BoardIdMemberDto,
+  ): string {
+    return this.commentService.create(createCommentDto, member); // 카드 ID와 멤버 권한을 검사하여 댓글 생성
   }
 
-  @Get()
+  @Patch('card/:cardId/comment/:commentId')
+  update(
+    @Param('cardId') cardId: string,
+    @Param('commentId') commentId: string,
+    @Body() updateCommentDto: UpdateCommentDto,
+    @Body() member: BoardIdMemberDto,
+  ) {
+    return this.commentService.update(+commentId, updateCommentDto);
+  }
+
+  @Get('')
   findAll() {
     return this.commentService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.commentService.findOne(+id);
+  @Get(':commentId')
+  findOne(@Param('commentId') commentId: string) {
+    return this.commentService.findOne(+commentId);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCommentDto: UpdateCommentDto) {
-    return this.commentService.update(+id, updateCommentDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.commentService.remove(+id);
+  @Delete('card/:cardId/comment/:commentId')
+  remove(
+    @Param('commentId') commentId: string,
+    @Body() member: BoardIdMemberDto,
+  ) {
+    return this.commentService.delete(+commentId);
   }
 }
+
+// function UserDetails(): (
+//   target: CommentController,
+//   propertyKey: 'createComment',
+//   parameterIndex: 2,
+// ) => void {
+//   throw new Error('Function not implemented.');
+// }
+
+// function User(): (
+//   target: CommentController,
+//   propertyKey: 'createComment',
+//   parameterIndex: 2,
+// ) => void {
+//   throw new Error('Function not implemented.');
+// }
