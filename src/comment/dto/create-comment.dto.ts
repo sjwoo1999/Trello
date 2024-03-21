702188c1c82c28927258a30abd5fff05abab136b
sinhong jeong <jsh719803@gmail.com>
2024-03-21 19:01:15 +0900

    🔨 CreateCommentDto 추가
    댓글 생성에 필요한 데이터를 나타내며,  댓글 내용, 작성자의 ID, 댓글이 연결된 카드의 ID 및 내용을 포함함.

--- src/comment/dto/create-comment.dto.ts
+++ src/comment/dto/create-comment.dto.ts
@@ -1,1 +1,5 @@
-export class CreateCommentDto {}
+export class CreateCommentDto {
+  readonly content: any; // 댓글 내용
+  readonly cardId: number; // 댓글이 연결된 카드의 ID
+  readonly userId: number; // 작성자의 ID
+}
